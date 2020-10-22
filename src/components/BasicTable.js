c37c2122d776cd363fe72545981b648aafc0fed7
Moti Elmakies <motiphone2003@gmail.com>
2020-10-22 23:54:38 +0300

    progress

---
+++ src/components/BasicTable.js
@@ -0,0 +1,16 @@
+import React, { useMemo } from 'react';
+import { useTable } from 'react-table';
+import MOCK_DATA from './MOCK_DATA.json';
+import { COLUMNS } from './Columns';
+
+const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+  const tableInstance = useTable({
+    columns: columns,
+    data: data,
+  });
+  return <div></div>;
+};
+
+export default BasicTable;
---
+++ src/components/Columns.js
@@ -0,0 +1,26 @@
+export const CLOUMNS = [
+  {
+    Header: 'Id',
+    accessor: ' id',
+  },
+  {
+    Header: 'First name',
+    accessor: 'first_name',
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'last_name',
+  },
+  {
+    Header: 'Date of Birth',
+    accessor: 'date_of_birth',
+  },
+  {
+    Header: 'Country',
+    accessor: 'country',
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone',
+  },
+];
